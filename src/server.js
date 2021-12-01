3e51be05ee37e9a7ddbbb4ef2a169c62422992a7
andrenogueiragbi <andrepereiragbi@gmail.com>
2021-12-01 18:18:03 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const morgan = require('morgan');
+const cors = require('cors')
 
 require('./database')
 
@@ -10,7 +11,10 @@ app.use(express.json());
 
 app.use(morgan('combined'));
 
+app.use(cors())
+
 app.use(routes);
+
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Rodando n porta 5000 \\O/.")
 })
